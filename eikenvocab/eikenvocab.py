1d8b38a03ab0ce6ad70c35573debe0979f7c1b8a
Christopher Bunn <github@thenerdery.org>
2021-06-19 17:45:44 +0900

    Change english_to_katakana to use different website, which requires using selenium.

--- eikenvocab/eikenvocab.py
+++ eikenvocab/eikenvocab.py
@@ -11,6 +11,15 @@ import csv
 import enchant
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from googletrans import Translator
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pykakasi
 
 # 1 - Convert PDFs to images
 # 2 - OCR images to text
@@ -73,16 +82,67 @@ def clean_wordlist(words: list) -> list:
 
 
 # 5 - Make list of most frequent words
-def get_most_frequent_words(words: list) -> list:
-    words = Counter(words).most_common(1000)
+def get_most_frequent_words(words: list, limit: int = 1000) -> list:
+    words = Counter(words).most_common(limit)
     return words
 
 
 # 6 - Transliterate English words with katakana
+def english_to_katakana(word: str) -> str:
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    # }
+    # url = f"https://www.sljfaq.org/cgi/e2k.cgi?word={word}"
+    # response = requests.get(url, headers=headers)
+    # # print(response.content)
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # katakana_pronunciation = soup.select_one("#katakana-string").text.strip()
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    # }
+    # url = f"https://japanga.com/name-converter#?q={word}"
+    # response = requests.get(url, headers=headers)
+    # # print(response.content)
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # katakana_pronunciation = soup.select_one(".name-katakana").text.strip()
+    url = f"https://japanga.com/name-converter#?q={word}"
+    max_delay = 10
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-gpu")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--window-size=1920,1080")
+    # webdriver_path = "/usr/bin/chromedriver"
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver.implicitly_wait(20)
+    driver.get(url)
+    try:
+        katakana_pronunciation = (
+            WebDriverWait(driver, max_delay)
+            .until(EC.visibility_of_element_located((By.CLASS_NAME, "nc-katakana")))
+            .text
+        )
+    finally:
+        driver.quit()
+    return katakana_pronunciation
+
 
 # 7 - Translate English words to Japanese (google)
+def english_to_japanese(word: str) -> str:
+    translator = Translator()
+    translation = translator.translate(word, src="en", dest="ja")
+    return translation.text
+
 
 # 8 - Transliterate Japanese into hiragana, katakana, romaji
+def japanese_to_hiragana(word: str) -> str:
+    kks = pykakasi.kakasi()
+    transliterationresult = kks.convert(word)
+    return transliterationresult[0]["hira"]
+
 
 # 9 - Output to CSV?
 
@@ -95,7 +155,21 @@ if __name__ == "__main__":
     # with open("output.txt", "w") as opened_file:
     #     opened_file.write(pdfs_to_string())
 
-    words = string_to_words(pdfs_to_string())
-    words = clean_wordlist(words)
-    words = get_most_frequent_words(words)
-    print(*words, sep=", ")
+    # words = string_to_words(pdfs_to_string())
+    # words = clean_wordlist(words)
+    # words = get_most_frequent_words(words)
+    words = [("mail", 1), ("sell", 1), ("something", 1), ("pet", 1), ("fever", 1)]
+    print(len(words))
+    print(type(words))
+    for wordcount in words:
+        print(wordcount)
+        print(type(wordcount))
+        word, count = wordcount
+        transliteration = english_to_katakana(word)
+        translation = english_to_japanese(word)
+        hiragana = japanese_to_hiragana(translation)
+        print(
+            f"Word: {word}, Count: {count}, Transliteration: {transliteration}, Translation: {translation}, Hiragana: {hiragana}"
+        )
+    # print(*words, sep=", ")
+    print(len(words))
