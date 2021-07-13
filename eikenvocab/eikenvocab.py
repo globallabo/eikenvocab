7a5fdcb03a829016be2ebfb19bc40e3154b81d82
Christopher Bunn <github@thenerdery.org>
2021-07-13 13:52:02 +0900

    Add docstrings for all functions.

--- eikenvocab/eikenvocab.py
+++ eikenvocab/eikenvocab.py
@@ -19,11 +19,19 @@ import pykakasi
 import jaconv
 
 
-# Use the text layer in the PDF instead of OCR
 def pdfs_to_string(
     input_path: str = Path(__file__).parent.parent.absolute() / "data/",
     drop_first_and_last_pages: bool = True,
 ) -> str:
+    """Extract the text layer of a PDF file.
+
+    Args:
+        input_path (str, optional): Path to the source PDF files. Defaults to Path(__file__).parent.parent.absolute()/"data/".
+        drop_first_and_last_pages (bool, optional): Whether to remove the first and last pages, which often don't have any content. Defaults to True.
+
+    Returns:
+        str: A string containing all of the text from the PDF files.
+    """
     # print("Starting PDF reading and text layer extraction ...")
     output_string = ""
     filelist = input_path.glob("*.pdf")
@@ -38,8 +46,15 @@ def pdfs_to_string(
     return output_string
 
 
-# 3 - Make list of all words
 def string_to_words(string: str) -> list[str]:
+    """Scrape all the individual words from a string of text.
+
+    Args:
+        string (str): The input string.
+
+    Returns:
+        list[str]: A list of all words, in lowercase.
+    """
     # the text results from the PDF often have a curly apostrophe, so replace it
     string = string.replace("’", "'")
     # include any words made of letters and the apostrophe, for contractions
@@ -47,16 +62,28 @@ def string_to_words(string: str) -> list[str]:
     return words
 
 
-# 4 - Clean up list of words
-# 4a - Remove single-character elements from the list
-def remove_single_character_elements(words: list) -> str:
+def remove_single_character_elements(words: list[str]) -> list[str]:
+    """Remove all single-character elements from a list of words (e.g. "a" and "I").
+
+    Args:
+        words (list[str]): A list of words to process.
+
+    Returns:
+        list[str]: A filtered list of words.
+    """
     words = [word for word in words if len(word) > 1]
     return words
 
 
-# 4b - Check against English spellcheck dictionaries
-#       to remove elements which are not words
-def filter_by_spellcheck(words: list) -> list:
+def filter_by_spellcheck(words: list[str]) -> list[str]:
+    """Filter out any words from a list that can't be found in a spellcheck dictionary, leaving only real English words.
+
+    Args:
+        words (list[str]): A list of words to process.
+
+    Returns:
+        list[str]: A filtered list of words.
+    """
     en_dictionary = enchant.Dict("en")
     us_dictionary = enchant.Dict("en_US")
     gb_dictionary = enchant.Dict("en_GB")
@@ -74,20 +101,45 @@ def filter_by_spellcheck(words: list) -> list:
     return words
 
 
-def clean_wordlist(words: list) -> list:
+def clean_wordlist(words: list[str]) -> list[str]:
+    """Clean up a list of words, to get a list of only real, useful English words.
+
+    Args:
+        words (list[str]): A list of words to process.
+
+    Returns:
+        list[str]: A filtered list of words.
+    """
     words = remove_single_character_elements(words)
     words = filter_by_spellcheck(words)
     return words
 
 
-# 5 - Make list of most frequent words
-def get_most_frequent_words(words: list, limit: Optional[int] = None) -> list:
+def get_most_frequent_words(
+    words: list[str], limit: Optional[int] = None
+) -> list[tuple]:
+    """Get a list of the most frequent words from a list of words.
+
+    Args:
+        words (list[str]): A list of words to process
+        limit (Optional[int], optional): A limit on how many words to output. Defaults to None.
+
+    Returns:
+        list[tuple]: A tuple containing a word and its frequency within the input list.
+    """
     words = Counter(words).most_common(limit)
     return words
 
 
-# 6 - Transliterate English words with katakana pronunciation
 def english_to_katakana(word: str) -> str:
+    """Transliterate an English word into its katakana pronunciation equivalent.
+
+    Args:
+        word (str): An English word.
+
+    Returns:
+        str: A katakana transliteration of the English word.
+    """
     url = "https://freeenglish.jp/convertp.php"
     mydata = {"englishtext": word, "prontype": "kana"}
 
@@ -100,8 +152,15 @@ def english_to_katakana(word: str) -> str:
     return katakana_pronunciation
 
 
-# 7 - Translate English words to Japanese (google)
 def english_to_japanese(word: str) -> str:
+    """Translate an English word into Japanese.
+
+    Args:
+        word (str): An English word.
+
+    Returns:
+        str: A Japanese translation of the English word.
+    """
     # translator = Translator()
     # translation = translator.translate(word, src="en", dest="ja")
     # return translation.text
@@ -114,8 +173,15 @@ def english_to_japanese(word: str) -> str:
     return translation["translatedText"]
 
 
-# 8 - Transliterate Japanese into hiragana, katakana, romaji
 def japanese_to_hiragana(word: str) -> str:
+    """Transliterate a Japanese string which may contain kanji into one which only contains hiragana.
+
+    Args:
+        word (str): A string of Japanese text.
+
+    Returns:
+        str: A string with all kanji transliterated into hiragana.
+    """
     kks = pykakasi.kakasi()
     transliterationresult = kks.convert(word)
     # the above result is tokenized by what kakasi thinks are
@@ -127,8 +193,13 @@ def japanese_to_hiragana(word: str) -> str:
     return hiragana
 
 
-# 9 - Output to Google Sheet (new worksheet)
 def write_gsheet(wordlist: list[dict], grade: str):
+    """Create a worksheet within a Google Sheets document and write to it a list of words and their pronunciations and translations. Any existing worksheet with the same name will first be backed up.
+
+    Args:
+        wordlist (list[dict]): A list of words, which are each a dictionary containing the fields like pronunciation and translation.
+        grade (str): The grade level of the word list.
+    """
     # sheetname = "Eiken Vocabulary"
     sheetname = "Eiken Vocabulary (testing)"
     max_rows = len(wordlist) + 10
